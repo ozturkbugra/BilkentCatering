b00c9b0743d039f39acbebd1113e00ddddbad0b8
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-01 16:00:18 +0300

    Add Hizmetlerimiz controller and view

---
+++ BilkentCatering.UI/Controllers/HizmetlerimizController.cs
@@ -0,0 +1,26 @@
+﻿using BilkentCatering.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BilkentCatering.UI.Controllers
+{
+    public class HizmetlerimizController : Controller
+    {
+        private readonly IServicesService _servicesService;
+
+        public HizmetlerimizController(IServicesService servicesService)
+        {
+            _servicesService = servicesService;
+        }
+
+        [Route("hizmetlerimiz")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Hizmetlerimiz";
+            ViewData["BodyClass"] = "starter-page-page";
+
+            var model = _servicesService.GetSingle();
+
+            return View(model);
+        }
+    }
+}
