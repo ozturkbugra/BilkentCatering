68cfb85f263ace5f4b72a1bb6da1d83100857b05
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-01 16:24:48 +0300

    Add IsBasvurusu controller and view

---
+++ BilkentCatering.UI/Controllers/IsBasvurusuController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BilkentCatering.UI.Controllers
+{
+    public class IsBasvurusuController : Controller
+    {
+        [Route("is-basvurusu")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
