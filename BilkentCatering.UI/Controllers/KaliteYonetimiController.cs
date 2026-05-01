9defb89cb77a54525a8fee650df003f72a7eed98
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-01 16:03:26 +0300

    Add KaliteYonetimi controller and view

---
+++ BilkentCatering.UI/Controllers/KaliteYonetimiController.cs
@@ -0,0 +1,26 @@
+﻿using BilkentCatering.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BilkentCatering.UI.Controllers
+{
+    public class KaliteYonetimiController : Controller
+    {
+        private readonly IQualityManagementService _qualityManagementService;
+
+        public KaliteYonetimiController(IQualityManagementService qualityManagementService)
+        {
+            _qualityManagementService = qualityManagementService;
+        }
+
+        [Route("kalite-yonetimi")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Kalite Yönetimi";
+            ViewData["BodyClass"] = "starter-page-page";
+
+            var policy = _qualityManagementService.GetSingle();
+
+            return View(policy);
+        }
+    }
+}
