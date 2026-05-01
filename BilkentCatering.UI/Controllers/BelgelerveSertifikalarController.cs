bd7fa09f84af685105a2ca3046b16d467a8dc558
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-01 15:57:19 +0300

    Add BelgelerveSertifikalar controller and view

---
+++ BilkentCatering.UI/Controllers/BelgelerveSertifikalarController.cs
@@ -0,0 +1,26 @@
+﻿using BilkentCatering.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BilkentCatering.UI.Controllers
+{
+    public class BelgelerveSertifikalarController : Controller
+    {
+        private readonly ICertificateAndDocumentService _certificateAndDocumentService;
+
+        public BelgelerveSertifikalarController(ICertificateAndDocumentService certificateAndDocumentService)
+        {
+            _certificateAndDocumentService = certificateAndDocumentService;
+        }
+
+        [Route("belgeler-ve-sertifikalar")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Belgeler ve Sertifikalar";
+            ViewData["BodyClass"] = "starter-page-page";
+
+            var model = _certificateAndDocumentService.GetAll();
+
+            return View(model);
+        }
+    }
+}
