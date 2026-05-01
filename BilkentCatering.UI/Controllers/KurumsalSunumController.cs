cd59db7d23c42b71a1e6de46cb48869ec7119dd3
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-01 16:11:40 +0300

    Add KurumsalSunum controller and view

---
+++ BilkentCatering.UI/Controllers/KurumsalSunumController.cs
@@ -0,0 +1,26 @@
+﻿using BilkentCatering.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BilkentCatering.UI.Controllers
+{
+    public class KurumsalSunumController : Controller
+    {
+        private readonly ICorporatePresentationService _corporatePresentationService;
+
+        public KurumsalSunumController(ICorporatePresentationService corporatePresentationService)
+        {
+            _corporatePresentationService = corporatePresentationService;
+        }
+
+        [Route("kurumsal-sunum")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Kurumsal Sunum";
+            ViewData["BodyClass"] = "starter-page-page";
+
+            var model = _corporatePresentationService.GetSingle();
+
+            return View(model);
+        }
+    }
+}
