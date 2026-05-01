72162822a8caf5ab910762ea8f0d2a528e84a427
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-01 15:49:42 +0300

    Add Politikamız controller and view

---
+++ BilkentCatering.UI/Controllers/PolitikamizController.cs
@@ -0,0 +1,26 @@
+﻿using BilkentCatering.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BilkentCatering.UI.Controllers
+{
+    public class PolitikamizController : Controller
+    {
+        private readonly IPolicyService _policyService;
+
+        public PolitikamizController(IPolicyService policyService)
+        {
+            _policyService = policyService;
+        }
+
+        [Route("politikamiz")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Politikamız";
+            ViewData["BodyClass"] = "starter-page-page";
+
+            var policy = _policyService.GetSingle();
+
+            return View(policy);
+        }
+    }
+}
