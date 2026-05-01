8c0301af510a62c3334948cd43cb60415bdfedb4
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-01 16:28:59 +0300

    Add Iletisim controller and view

---
+++ BilkentCatering.UI/Controllers/IletisimController.cs
@@ -0,0 +1,16 @@
+﻿using BilkentCatering.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BilkentCatering.UI.Controllers
+{
+    public class IletisimController : Controller
+    {
+        [Route("iletisim")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "İletişim";
+            ViewData["BodyClass"] = "starter-page-page";
+            return View();
+        }
+    }
+}
