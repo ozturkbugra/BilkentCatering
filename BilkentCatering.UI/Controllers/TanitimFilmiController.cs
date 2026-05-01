cf3b0a4325893a0beeb0dabebc4823b5e8ece4d6
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-01 16:16:17 +0300

    Add TanitimFilmi controller and view

---
+++ BilkentCatering.UI/Controllers/TanitimFilmiController.cs
@@ -0,0 +1,26 @@
+﻿using BilkentCatering.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BilkentCatering.UI.Controllers
+{
+    public class TanitimFilmiController : Controller
+    {
+        private readonly IIntroductionVideoService _introductionVideoService;
+
+        public TanitimFilmiController(IIntroductionVideoService introductionVideoService)
+        {
+            _introductionVideoService = introductionVideoService;
+        }
+
+        [Route("tanitim-filmi")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Tanıtım Filmi";
+            ViewData["BodyClass"] = "starter-page-page";
+
+            var model = _introductionVideoService.GetSingle();
+
+            return View(model);
+        }
+    }
+}
