65b475086411d2068d2d3005029cf3b2f1aa8dca
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-01 09:35:15 +0300

    Add JobApplication controller and view

---
+++ BilkentCatering.UI/Areas/Admin/Controllers/JobApplicationController.cs
@@ -0,0 +1,59 @@
+﻿using BilkentCatering.Business.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BilkentCatering.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(AuthenticationSchemes = "AdminCookie")]
+    public class JobApplicationController : Controller
+    {
+        private readonly IJobApplicationService _jobApplicationService;
+
+        public JobApplicationController(IJobApplicationService jobApplicationService)
+        {
+            _jobApplicationService = jobApplicationService;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "İş Başvuruları";
+            ViewData["Breadcrumb"] = "İş Başvuruları";
+            var applications = _jobApplicationService.GetAll().Where(x => !x.IsDeleted)
+                .OrderByDescending(x => x.ApplicationDate).ToList();
+            return View(applications);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ToggleRead(int id)
+        {
+            var application = _jobApplicationService.GetById(id);
+            if (application == null)
+            {
+                TempData["Error"] = "Başvuru bulunamadı.";
+                return RedirectToAction("Index");
+            }
+
+            application.IsRead = !application.IsRead;
+            _jobApplicationService.Update(application);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            var application = _jobApplicationService.GetById(id);
+            if (application == null)
+            {
+                TempData["Error"] = "Başvuru bulunamadı.";
+                return RedirectToAction("Index");
+            }
+
+            var result = _jobApplicationService.Delete(application);
+            TempData[result.Success ? "Success" : "Error"] = result.Message;
+            return RedirectToAction("Index");
+        }
+    }
+}
