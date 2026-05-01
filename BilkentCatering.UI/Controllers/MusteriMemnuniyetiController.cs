b907d657f4107996835b321db603a206728ddcb9
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-01 16:06:39 +0300

    Add MusteriMemnuniyeti controller and view

---
+++ BilkentCatering.UI/Controllers/MusteriMemnuniyetiController.cs
@@ -0,0 +1,26 @@
+﻿using BilkentCatering.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BilkentCatering.UI.Controllers
+{
+    public class MusteriMemnuniyetiController : Controller
+    {
+        private readonly ICustomerSatisfactionService _customerSatisfactionService;
+
+        public MusteriMemnuniyetiController(ICustomerSatisfactionService customerSatisfactionService)
+        {
+            _customerSatisfactionService = customerSatisfactionService;
+        }
+
+        [Route("musteri-memnuniyeti")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Müşteri Memnuniyeti";
+            ViewData["BodyClass"] = "starter-page-page";
+
+            var policy = _customerSatisfactionService.GetSingle();
+
+            return View(policy);
+        }
+    }
+}
