0e5905455d6c6a09c61f7d3dec222dc11250ea26
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-01 20:47:29 +0300

    Fixed seo error

---
+++ BilkentCatering.UI/ViewComponents/SeoViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using BilkentCatering.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BilkentCatering.UI.ViewComponents
+{
+    public class SeoViewComponent : ViewComponent
+    {
+        private readonly ISiteSettingsService _siteSettingsService;
+
+        public SeoViewComponent(ISiteSettingsService siteSettingsService)
+        {
+            _siteSettingsService = siteSettingsService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _siteSettingsService.GetSingle();
+            return View(model);
+        }
+    }
+}
