83a5d931168a2c8967346a512fd510ea8ced2d7e
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-05-01 20:08:17 +0300

    Add mesaj controller and gonder method

---
+++ BilkentCatering.UI/Controllers/MesajController.cs
@@ -0,0 +1,41 @@
+﻿using BilkentCatering.Business.Abstract;
+using BilkentCatering.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BilkentCatering.UI.Controllers
+{
+    public class MesajController : Controller
+    {
+        private readonly IMessageService _messageService;
+
+        public MesajController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Gonder(Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _messageService.Add(message);
+
+                if (result != null)
+                {
+                    // Başarılı durumunda JSON dönüyoruz
+                    return Json(new { success = true, message = "Mesajınız başarıyla iletildi. En kısa sürede size dönüş yapacağız." });
+                }
+                else
+                {
+                    // Servis hatası
+                    return Json(new { success = false, message = "Mesaj gönderilirken bir hata oluştu." });
+                }
+            }
+
+            // Validasyon hatası
+            return Json(new { success = false, message = "Lütfen formdaki zorunlu alanları eksiksiz doldurun." });
+        }
+    }
+}
+
